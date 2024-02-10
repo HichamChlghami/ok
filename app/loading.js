617d89e8b841aa4a2d032e6ca71466e08c27b683
Hicham <hichamimad39@gmail.com>
2024-02-10 04:05:09 -0800

    the first one

---
+++ app/loading.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import './Loading.css'; // Import your CSS file
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loader">
+        <div className="dot"></div>
+        <div className="dot1"></div>
+        <div className="dot2"></div>
+      </div>
+      <h1 className='loader-text'>Loading...</h1>
+    </div>
+  );
+}
+
+export default Loading;
+
+
+
+
